64f9e8da6db9e0d282d38920b6bb67d95880a0c8
Plan A <cookiessaa@gmail.com>
2026-09-06 09:26:31 +0300

    Add KaiCleave submodule bootstrap

---
+++ ci/KaiCleave/SubModule.cs
@@ -0,0 +1,36 @@
+using HarmonyLib;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace KaiCleave
+{
+    public sealed class SubModule : MBSubModuleBase
+    {
+        internal const string HarmonyId = "kai.cleave.bannerlord.1.3.15";
+        internal const string Version = "0.1.0-beta";
+
+        private Harmony _harmony;
+
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+
+            _harmony = new Harmony(HarmonyId);
+            _harmony.PatchAll(typeof(SubModule).Assembly);
+
+            InformationManager.DisplayMessage(
+                new InformationMessage("[KaiCleave] " + Version + " loaded | Bannerlord 1.3.15 / TOR"));
+        }
+
+        protected override void OnSubModuleUnloaded()
+        {
+            if (_harmony != null)
+            {
+                _harmony.UnpatchAll(HarmonyId);
+                _harmony = null;
+            }
+
+            base.OnSubModuleUnloaded();
+        }
+    }
+}
