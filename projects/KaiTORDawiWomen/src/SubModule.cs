2fb15db2bf953fff0a9eefc221608d5636d397f3
Plan A <cookiessaa@gmail.com>
2026-09-15 22:02:07 +0300

    DawiWomen: add optional module entry point

---
+++ projects/KaiTORDawiWomen/src/SubModule.cs
@@ -0,0 +1,12 @@
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace KaiTOR.DawiWomen;
+
+public sealed class SubModule : MBSubModuleBase
+{
+    protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+    {
+        base.OnGameStart(game, gameStarterObject);
+    }
+}
