c432b32703b56557c7f843465fe286461f1f2659
Plan A <cookiessaa@gmail.com>
2026-09-15 07:26:14 +0300

    KaiTOR Diplomacy: install TOR-safe models and culture behavior

--- projects/KaiTORDiplomacy/src/SubModule.cs
+++ projects/KaiTORDiplomacy/src/SubModule.cs
@@ -1,5 +1,8 @@
+using System;
+using KaiTOR.Diplomacy.Models;
 using KaiTOR.Diplomacy.Runtime;
 using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.ComponentInterfaces;
 using TaleWorlds.Core;
 using TaleWorlds.MountAndBlade;
 
@@ -11,9 +14,31 @@ public sealed class SubModule : MBSubModuleBase
     {
         base.OnGameStart(game, gameStarterObject);
 
-        if (gameStarterObject is CampaignGameStarter campaignStarter)
-        {
-            campaignStarter.AddBehavior(new KaiDiplomacyBehavior());
-        }
+        if (gameStarterObject is not CampaignGameStarter campaignStarter)
+            return;
+
+        InstallPermissionWrapper(campaignStarter);
+        InstallMarriageWrapper(campaignStarter);
+
+        campaignStarter.AddBehavior(new KaiDiplomacyBehavior());
+        campaignStarter.AddBehavior(new KaiCultureAssimilationBehavior());
+    }
+
+    private static void InstallPermissionWrapper(CampaignGameStarter starter)
+    {
+        var torModel = starter.GetModel<KingdomDecisionPermissionModel>();
+        if (!string.Equals(torModel?.GetType().FullName, KaiKingdomDecisionPermissionModel.ExpectedTorBaseType, StringComparison.Ordinal))
+            return;
+
+        starter.AddModel<KingdomDecisionPermissionModel>(new KaiKingdomDecisionPermissionModel(torModel));
+    }
+
+    private static void InstallMarriageWrapper(CampaignGameStarter starter)
+    {
+        var torModel = starter.GetModel<MarriageModel>();
+        if (!string.Equals(torModel?.GetType().FullName, KaiPlayerMarriageModel.ExpectedTorBaseType, StringComparison.Ordinal))
+            return;
+
+        starter.AddModel<MarriageModel>(new KaiPlayerMarriageModel(torModel));
     }
 }
