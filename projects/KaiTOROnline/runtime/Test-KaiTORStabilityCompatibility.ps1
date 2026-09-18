d7087c69d14204383f4b3be2e217b6232d4353a7
Plan A <cookiessaa@gmail.com>
2026-09-19 00:22:33 +0300

    KaiTOR Online: validate current Stability v0.5.0

--- projects/KaiTOROnline/runtime/Test-KaiTORStabilityCompatibility.ps1
+++ projects/KaiTOROnline/runtime/Test-KaiTORStabilityCompatibility.ps1
@@ -9,7 +9,7 @@ $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
 
 $ExpectedModuleId = 'KaiTOR_Stability'
-$ExpectedVersion = 'v0.4.1'
+$ExpectedVersion = 'v0.5.0'
 $ExpectedGameVersion = 'v1.3.15'
 
 $root = (Resolve-Path -LiteralPath $BannerlordRoot).Path
