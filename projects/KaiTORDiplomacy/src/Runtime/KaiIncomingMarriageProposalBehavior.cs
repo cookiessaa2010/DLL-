123427ca0f87c3ea2e2df331dfc026e208dbed0e
Plan A <cookiessaa@gmail.com>
2026-09-19 07:01:21 +0300

    Fix Phase A marriage inquiry namespaces

--- projects/KaiTORDiplomacy/src/Runtime/KaiIncomingMarriageProposalBehavior.cs
+++ projects/KaiTORDiplomacy/src/Runtime/KaiIncomingMarriageProposalBehavior.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.Party;
 using TaleWorlds.Core;
+using TaleWorlds.Library;
 using TaleWorlds.Localization;
 
 namespace KaiTOR.Diplomacy.Runtime;
