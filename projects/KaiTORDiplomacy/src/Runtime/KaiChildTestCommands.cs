b831ae8620cacc7f7524bf2275780b63dda3d467
Plan A <cookiessaa@gmail.com>
2026-09-22 18:13:04 +0300

    v0.6.7: fix child test spawner party namespace

--- projects/KaiTORDiplomacy/src/Runtime/KaiChildTestCommands.cs
+++ projects/KaiTORDiplomacy/src/Runtime/KaiChildTestCommands.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Party;
 using TaleWorlds.Core;
 using TaleWorlds.Library;
 using TaleWorlds.Localization;
